ef0a48887dde2fab903bc2478bf1ba6d1b73bfe4
Tom Smith <thomas_smith@live.ca>
2020-05-18 23:56:46 -0600

    improved messaging

--- blackjack.js
+++ blackjack.js
@@ -12,77 +12,76 @@ const stand = /^stand$/gi;
 // TODO
 // use map to track multiple games at once
 // each channel can have 1 active game
-const game = {
-  activePlayer: null,
-  channel: null,
-  dealer: { name: 'Dealer', cards: [] },
-  deck: cards.shuffledDeck(),
-  players: [],
-  state: 'pending',
-};
+let currGame = game();
 
 async function messageHandler(msg) {
   try {
-    switch (game.state) {
+    switch (currGame.state) {
     case 'pending':
       if (startText !== msg.cleanContent)
         break;
 
-      game.channel = msg.channel;
-      game.state = 'starting';
+      currGame.channel = msg.channel;
+      currGame.state = 'starting';
 
+      // TODO
+      // look up channel nickname for player name
       for (const player of await getPlayers(msg.channel))
-        // TODO
-        // look up channel nickname for player name
-        game.players.push({ user: player, name: player.username, cards: [], bet: 0, cash: 300 });
+        currGame.players.push({ user: player, name: player.username, cards: [], bet: 0, cash: 300 });
 
-      await game.channel.send({ embed: embedMessage([game.dealer].concat(game.players), 'Game Start!') });
-      await game.channel.send({ embed: embedMessage([], 'Place your bets', '', `Send '1234' to place a bet.`) });
+      if (currGame.players.length === 0) {
+        await currGame.channel.send({ embed: embedMessage([], 'Game cannot start without players!', 'Make sure to click the die to join the game') });
+        currGame = game();
+      }
 
-      game.state = 'betting';
+      await currGame.channel.send({ embed: embedMessage([], 'Game Start!') });
+      await currGame.channel.send({ embed: embedMessage([currGame.dealer].concat(currGame.players), 'Place your bets', '', 'Send 1234 to place a bet.') });
+
+      currGame.state = 'betting';
 
       setTimeout(async () => {
-        for (const p of game.players) {
-          p.cards.push(game.deck.pop());
-          p.cards.push(game.deck.pop());
+        for (const p of currGame.players) {
+          p.cards.push(currGame.deck.pop());
+          p.cards.push(currGame.deck.pop());
         }
-        game.dealer.cards.push(game.deck.pop());
-        game.dealer.cards.push(game.deck.pop());
+        currGame.dealer.cards.push(currGame.deck.pop());
+        currGame.dealer.cards.push(currGame.deck.pop());
 
-        game.state = 'playing';
-        game.activePlayer = 0;
-        await game.channel.send({ embed: embedMessage([game.dealer].concat(game.players), 'Betting Concluded') });
-        await game.channel.send('Hit or Stand?');
-      }, 1000);
+        currGame.state = 'playing';
+        currGame.activePlayer = 0;
+        await currGame.channel.send({ embed: embedMessage([currGame.dealer].concat(currGame.players), 'Betting has finished.') });
+        await currGame.channel.send({ embed: embedMessage([currGame.players[currGame.activePlayer]], `${currGame.players[currGame.activePlayer].name}, Hit or Stand?`) });
+      }, 10000);
       break;
 
     case 'betting':
-
+      // TODO
+      // update player's bets and notify that bets have been received
       break;
 
     case 'playing':
-      if (msg.author !== game.players[game.activePlayer].user)
+      if (msg.author !== currGame.players[currGame.activePlayer].user)
         return;
 
       if (hit.test(msg.content)) {
-        game.players[game.activePlayer].cards.push(game.deck.pop());
+        currGame.players[currGame.activePlayer].cards.push(currGame.deck.pop());
 
-        if (!isValid(game.players[game.activePlayer].cards)) {
-          game.channel.send({ embed: embedMessage(game.players[game.activePlayer], 'Bust') });
-          game.activePlayer++;
+        if (!isValid(currGame.players[currGame.activePlayer].cards)) {
+          currGame.channel.send({ embed: embedMessage([currGame.players[currGame.activePlayer]], `${currGame.players[currGame.activePlayer].name}, bust!`, 'Better luck next time.') });
+          currGame.activePlayer++;
         }
       }
 
-      if (stand.test())
-        game.activePlayer++;
+      if (stand.test(msg.content))
+        currGame.activePlayer++;
 
-      if (game.activePlayer >= game.players.length) {
+      if (currGame.activePlayer >= currGame.players.length) {
         hitDealer();
-        await finish(game);
+        await finish(currGame);
         break;
       }
 
-      await game.channel.send({ embed: embedMessage(game.players[game.activePlayer], `${game.players[game.activePlayer].name}, hit or stand?`) });
+      await currGame.channel.send({ embed: embedMessage([currGame.players[currGame.activePlayer], currGame.dealer], `${currGame.players[currGame.activePlayer].name}, hit or stand?`) });
       break;
     }
   }
@@ -92,23 +91,14 @@ async function messageHandler(msg) {
 }
 
 async function getPlayers(channel) {
-  const startingMsg = await channel.send({
-    embed: new discord.MessageEmbed({
-      title: 'A game of Blackjack is starting in 20 seconds!',
-      color: 'LUMINOUS_VIVID_PINK',
-      description: 'Click the die to join',
-    }),
-  });
+  const startingMsg = await channel.send({ embed: embedMessage([], 'A game of Blackjack is starting in 20 seconds!', 'Click the die to join.') });
   await startingMsg.react('🎲');
 
-  const reactions = await startingMsg.awaitReactions((r) => r.emoji.name === '🎲', { time: 5000 });
-
-  const diceEmoji = reactions.get('🎲');
+  const diceEmoji = (await startingMsg.awaitReactions((r) => r.emoji.name === '🎲', { time: 5000 })).get('🎲');
   if (!diceEmoji)
     return [];
 
-  const us = await diceEmoji.users.fetch();
-  return us.filter(u => !u.bot);
+  return (await diceEmoji.users.fetch()).filter(u => !u.bot).values();
 }
 
 function isValid(hand) {
@@ -122,10 +112,12 @@ function isValid(hand) {
 async function hitDealer() {
   // use blackjack rules to decide if dealer hits
   // send message for what dealer does
+  // dealer hits if bellow 17, stands if 17 or more
+  // aces are counted at 11 for dealer
 }
 
 async function finish(aGame) {
-  await game.channel.send({ embed: embedMessage([game.dealer].concat(game.players), 'Game finished!') });
+  await currGame.channel.send({ embed: embedMessage([currGame.dealer].concat(currGame.players), 'Game finished!') });
   // determine winners
   // calculate payouts
   // display results
@@ -133,11 +125,28 @@ async function finish(aGame) {
 }
 
 function embedMessage(players, title, description, footer) {
-  const msg = discord.MessageEmbed({ title, description, footer });
+  const msg = new discord.MessageEmbed({ title: title || '', description: description || '', footer: { text: footer || '' }, color: 'LUMINOUS_VIVID_PINK' });
+
   for (const p of players)
     msg.addField(
       p.name,
-      `Hand:${p.cards.length ? p.cards.map(cards.toString).join('|') : 'empty'}
-      Bet:$${p.bet}
-      Cash:$${p.cash}`);
+      // TODO
+      // don't show the dealers cards when the game is being played
+      `${p.cards ? 'Hand:' + p.cards.length ? p.cards.map(cards.toEmoji).join('|') : 'empty' : 'no hand'}
+      ${p.bet ? 'Bet:$' + p.bet : 'no bet'}
+      ${p.cash ? 'Cash:$' + p.cash : 'no cash'}`,
+      true);
+
+  return msg;
+}
+
+function game() {
+  return {
+    activePlayer: null,
+    channel: null,
+    dealer: { name: 'Dealer', cards: [] },
+    deck: cards.shuffledDeck(),
+    players: [],
+    state: 'pending',
+  };
 }
